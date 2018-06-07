93511628515b66d55e6134dbacf15d5135534dc3
Hang Yu <hvy5146@psu.edu>
2018-06-07 15:06:02 +0800

    adding thread

--- src/login.py
+++ src/login.py
@@ -1,7 +1,8 @@
 import requests
+from threading import Thread
 domain = '10.176.111.32:8080'
 cookie_path = ''
-
+return_val = 0
 def stringtolist(s):
     return s.split(',')
 
@@ -10,23 +11,9 @@ def get_session_info(tup):
     t2 = tup[1].split('"')
     return t1[5]+'='+ t2[3]
 
-def read_cookie():
-    cookie = ''
-    with open(cookie_path+"cookie.txt","r") as f:
-        cookie = f.read()
-    return cookie
-
-""" This function sign a user if un and pw are both correct
-    Return 1 on success, 0 on failure """
-
-def login(lst):
-    un = lst[0]
-    pw = lst[1]
-    url = 'http://'+domain+'/rest/auth/1/session'
-    data = '{"username":"'+str(un)+'","password":"'+str(pw)+'"}'
-    headers = {'Content-Type':'application/json'}
+def send_request(url, data, headers):
     try:
-        r = requests.post(url,headers=headers,data=data,timeout = 1)
+        r = r = requests.post(url,headers=headers,data=data,timeout = 2)
         if(r.status_code == 200):
             s = stringtolist(r.text)
             cookie = get_session_info((s[0],s[1]))
@@ -34,10 +21,24 @@ def login(lst):
             f.write(cookie)
             f.close
             print(r.text)
-            return 1
-        print("Error Occured! Status Code:",r.status_code)
-        print(r.text)
+            global return_val
+            return_val = 1
+        else:
+            print("Error Occured! Status Code:",r.status_code)
+            print(r.text)
     except requests.exceptions.ConnectTimeout as err:
         print(err)
-    return 0
-login(["admin","admin"])
+""" This function sign a user if un and pw are both correct
+    Return 1 on success, 0 on failure """
+
+def login(lst):
+
+    un = lst[0]
+    pw = lst[1]
+    url = 'http://'+domain+'/rest/auth/1/session'
+    data = '{"username":"'+str(un)+'","password":"'+str(pw)+'"}'
+    headers = {'Content-Type':'application/json'}
+    thread = Thread(target = send_request, args=(url, data, headers))
+    thread.start()
+    thread.join()
+    return return_val

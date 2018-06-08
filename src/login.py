80a6d2caa40c3c3bc02bd1efcb33df2736917351
Hang Yu <hvy5146@psu.edu>
2018-06-08 09:12:51 +0800

    change

--- src/login.py
+++ src/login.py
@@ -3,6 +3,7 @@ from threading import Thread
 domain = '10.176.111.32:8080'
 cookie_path = ''
 return_val = 0
+finish = 0
 def stringtolist(s):
     return s.split(',')
 
@@ -11,33 +12,24 @@ def get_session_info(tup):
     t2 = tup[1].split('"')
     return t1[5]+'='+ t2[3]
 
-def send_request(url, data, headers):
-    try:
-        r = r = requests.post(url,headers=headers,data=data,timeout = 2)
-        if(r.status_code == 200):
-            s = stringtolist(r.text)
-            cookie = get_session_info((s[0],s[1]))
-            f = open(cookie_path+"cookie.txt","w")
-            f.write(cookie)
-            f.close
-            print(r.text)
-            global return_val
-            return_val = 1
-        else:
-            print("Error Occured! Status Code:",r.status_code)
-            print(r.text)
-    except requests.exceptions.ConnectTimeout as err:
-        print(err)
+
 """ This function sign a user if un and pw are both correct
     Return 1 on success, 0 on failure """
-
 def login(lst):
     un = lst[0]
     pw = lst[1]
     url = 'http://'+domain+'/rest/auth/1/session'
     data = '{"username":"'+str(un)+'","password":"'+str(pw)+'"}'
     headers = {'Content-Type':'application/json'}
-    thread = Thread(target = send_request, args=(url, data, headers))
-    thread.start()
-    thread.join()
-    return return_val
+    r = requests.post(url,headers=headers,data=data)
+    if(r.status_code == 200):
+        s = stringtolist(r.text)
+        cookie = get_session_info((s[0],s[1]))
+        f = open(cookie_path+"cookie.txt","w")
+        f.write(cookie)
+        f.close
+        print(r.text)
+        return 1
+    print("Error Occured! Status Code:",r.status_code)
+    print(r.text)
+    return 0

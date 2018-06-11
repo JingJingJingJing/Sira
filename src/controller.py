45dfff21db1236fc053b0b249bbc1cb3cb97bddd
Xiapeng XP2 Zheng <zhengxp2@Lenovo.com>
2018-06-11 17:26:46 +0800

    fix bugs

--- src/controller.py
+++ src/controller.py
@@ -22,7 +22,7 @@ class SiraController():
         return self.cal(string)
 
     def cal(self, command):
-        if(len(command) <= 0):
+        if(len(command) <= 0 and (self.position is None or self.position == self.tree)):
             self.view.set_command_mode(True)
             self.view.info = [self.cursor]
             return
@@ -39,11 +39,12 @@ class SiraController():
                     self.position = child
                     break
                 elif (child.tag == "optional" or child.tag == "required"):
-                    self.position = child
-                    self.paras.append(token)
+                    if(token):
+                        self.position = child
+                        self.paras.append(token)
                     break
             # no keyword paired, return error
-            if(pre_position == self.position):
+            if(pre_position == self.position and token):
                 self.clearcache()
                 self.view.set_command_mode(True)
                 self.view.info = ["error command", self.cursor]
@@ -67,29 +68,43 @@ class SiraController():
             return
         elif(self.position.find("./keyword") is not None):
             interactive = self.position.find("./interactive")
-            self.view.set_command_mode(True)
             if(interactive is not None):
+                self.view.set_command_mode(False)
                 self.view.info = [interactive.text, self.interactive_cursor]
                 return
             else:
+                self.view.set_command_mode(True)
                 self.clearcache()
                 self.view.info = ["error command", self.cursor]
                 return
         else:
             self.view.commandText.readonly = True
-            threading.Thread(None,self.execfunc).start()
+            functag = self.position.find("./function")
+            if(functag.attrib['multi-thread'] == 'True'):
+                threading.Thread(None,self.execfunc).start()
+            else:
+                self.execfunc()
             return
 
     def execfunc(self):
         functag = self.position.find("./function")
-        result = getattr(eval(functag.attrib['object']), functag.attrib['name'])(self.paras)
+        if(self.paras):
+            result = getattr(eval(functag.attrib['object']), functag.attrib['name'])(self.paras)
+        else:
+            result = getattr(eval(functag.attrib['object']), functag.attrib['name'])()
             # set cursor value to username when login successd
-        if(functag.attrib['object'] == "login" and "errorMessages" not in result):
-            self.cursor = self.paras[0] + ">"
-        elif(functag.attrib['object'] == "login" and "errorMessages" in result):
+        if(functag.attrib['object'] == "login" and result[0] == 1):
+            self.cursor = self.paras[0] + self.cursor
+        elif(functag.attrib['object'] == "login" and result[0] == 0):
             self.cusor = self.normal_cursor
         self.view.commandText.readonly = False
-        self.view.info = [result, self.cursor]
+        if(result):
+            if(functag.attrib['object'] == "login"):
+                self.view.info = [result[1], self.cursor]
+            else:
+                self.view.info = [result, self.cursor]
+        else:
+            self.view.info = [self.cursor]
         self.clearcache()
         self.view.set_command_mode(True)
 

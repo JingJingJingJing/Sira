f76bc7fafea0c12c3aeded1a4baf22cd5de6886c
Hang Yu <hvy5146@psu.edu>
2018-06-12 11:26:24 +0800

    query.py version3.0

--- src/query.py
+++ src/query.py
@@ -4,7 +4,8 @@ import json
 from extract import getIssue
 from extract import getField
 from extract import dtos
-
+from login import method
+from login import send_request
 domain = '10.176.111.32:8080'
 cookie_path = ''
 
@@ -16,200 +17,139 @@ def read_cookie():
 
 """ This function returns all issue assigned to the user 'user' """
 
-def query(f, data):
-    cookie = read_cookie()
+
+def query(field1, field2, f):
+    cookie = ''
+    try:
+        cookie = read_cookie()
+    except FileNotFoundError as err:
+        return err
     url = 'http://'+domain+'/rest/api/2/search'
     headers = {'Content-Type':'application/json','cookie':cookie}
-    data = '{"jql":"'+f+'='+data+'","startAt":0, "maxResults": 100,"fields":["summary","issuetype","project","fixVersions","assignee","status"]}'
+    data = '{"jql":"'+field1
+    if f:
+        data += ' and '+field2+'","startAt":0, "maxResults": 100,"fields":["summary","issuetype","project","fixVersions","assignee","status"]}'
+    else:
+        data += '","startAt":0, "maxResults": 100,"fields":["summary","issuetype","project","fixVersions","assignee","status"]}'
+    flag,r = send_request(url, method.Post, headers, None, data)
+    if not flag:
+        return r
+
+    j = json.loads(r.text)
     try:
-        r = requests.post(url,headers=headers,data=data,timeout = 3)
-        if(r.status_code == 200):
-            j = json.loads(r.text)
+        return j['warningMessages']
+    except KeyError:
+        pass
+    issue_lst = getIssue(r.text, None)
+    if len(issue_lst) > 0:
+        string = ''
+        for i in range(0, len(issue_lst)):
             try:
-                return j['warningMessages']
-            except KeyError:
-                pass
-            issue_lst = getIssue(r.text, None)
+                if i == len(issue_lst)-1:
+                    string += dtos(getField(issue_lst[i],None),issue_lst[i]['key'])
+                else:
+                    string += dtos(getField(issue_lst[i],None),issue_lst[i]['key']) + '\r\n'
+            except KeyError as err:
+                return_val = 'given field "{}" not found'.format(err)
+                return return_val
+        print(string)
+        return string
+    else:
+        return 'Issue not Found'
 
-            if len(issue_lst) > 0:
-                string = ''
-                for i in issue_lst:
-                    try:
-                        string += dtos(getField(i,None),i['key']) + '\r\n'
-                    except KeyError as err:
-                        return_val = 'given field "{}" not found'.format(err)
-                        return return_val
-                return string
-            else:
-                return 'Issue not Found'
-        else:
-            return str(r.status_code) + "Failed"
-    except requests.exceptions.RequestException as err:
-        return err
-
-def finduser(user):
-    cookie = read_cookie()
-    url = 'http://'+domain+'/rest/api/2/user/search'
-    headers = {'Content-Type':'application/json','cookie':cookie}
-    data={'username':user}
-    try:
-        r = requests.get(url,headers=headers,params=data,timeout=3)
-        if r.status_code != 200:
-            return (0,str(r.status_code) + r.text)
-        j = json.loads(r.text)
-        try:
-            return (1,j[0]['key'])
-        except KeyError:
-            return (0,'No user found!')
-        except IndexError:
-            return (0,'No user found!')
-    except requests.exceptions.RequestException as err:
-        return (0,err)
 
 """ This function will return all information of issue represented by pid """
 
 def query_number(lst):
-    if len(lst) == 0:
-        return None
     issue = lst[0]
-    cookie = read_cookie()
+    cookie = ''
+    try:
+        cookie = read_cookie()
+    except FileNotFoundError as err:
+        return err
+
     url = 'http://'+domain+'/rest/api/2/issue/'+issue
     headers = {'Content-Type':'application/json','cookie':cookie}
+    f,r = send_request(url, method.Get, headers, None, None)
+    if not f:
+        return r
+    j = json.loads(r.text)
     try:
-        r = requests.get(url,headers=headers,timeout=3)
-        if r.status_code != 200:
-            return str(r.status_code) + "Failed"
-        j = json.loads(r.text)
+        return j['warningMessages']
+    except KeyError:
         try:
-            return j['warningMessages']
-        except KeyError:
-            pass
-        try:
-            string = dtos(getField(j,None),j['key']) + '\r\n'
-            return string
+            return dtos(getField(j,None),j['key'])
         except KeyError as err:
-            return_val = 'given field "{}" not found'.format(err)
-            return return_val
-    except requests.exceptions.RequestException as err:
-        return err
+            return 'given field "{}" not found'.format(err)
+
+
 
 def query_sprint(lst):
-    if len(lst) == 0:
-        return None
-    sp = lst[0]
-    return query('sprint', sp)
+    return query('sprint='+ lst[0],'',0)
     
 def query_assignee(lst):
-    if len(lst) == 0:
-        return None
-    
     sign, data = finduser(lst[0])
     if sign:
-        return query('assignee', data)
+        return query('assignee='+ data,'',0)
     else:
         return data
-
-def query_project(data,p,t):
-    cookie = read_cookie()
-    url = 'http://'+domain+'/rest/api/2/search'
-    headers = {'Content-Type':'application/json','cookie':cookie}
-    data = '{"jql":"project='+data+' and '+p+'='+t+'","startAt":0, "maxResults": 100,"fields":["summary","issuetype","project","fixVersions","assignee","status"]}'
-    try:
-        r = requests.post(url,headers=headers,data=data,timeout = 3)
-        if(r.status_code == 200):
-            
-            j = json.loads(r.text)
-            try:
-                return j['warningMessages']
-            except KeyError:
-                
-                pass
-            issue_lst = getIssue(r.text, None)
-
-            if len(issue_lst) > 0:
-                string = ''
-                for i in issue_lst:
-                    try:
-                        string += dtos(getField(i,None),i['key']) + '\r\n'
-                    except KeyError as err:
-                        
-                        return_val = 'given field "{}" not found'.format(err)
-                        
-                        return return_val
-                print(string)
-                return string
-            else:
-                
-                return 'Issue not Found'
-        else:
-            return str(r.status_code) + "Failed"
-    except requests.exceptions.RequestException as err:
-        return err
-
-
-def query_project_type(lst):
-    return query_project(lst[0],'issuetype',lst[1])
-def query_project_user(lst):
-    sign, data = finduser(lst[1])
-    if sign:
-        return query_project(lst[0],'assignee',data)
-    else:
-        return data
-def query_project_sprint(lst):
-    return query_project(lst[0],'sprint',lst[1])
-    
-
 
 def query_type(lst):
-    if len(lst) == 0:
-        return None
-    itype = lst[0]
-    return query('issuetype', itype)
+    return query('issuetype ='+ lst[0],'',0)
+
+def query_project_type(lst):
+    return query('project='+lst[0],'issuetype ='+lst[1],1)
+
+def query_project_assignee(lst):
+    sign, data = finduser(lst[1])
+    if sign:
+        return query('project ='+lst[0],'assignee ='+data,1)
+    else:
+        return data
+
+def query_project_sprint(lst):
+    return query('project ='+lst[0],'sprint ='+lst[1],1)
+    
 
 
-def getBoard():
-    cookie = read_cookie()
-    url = 'http://10.176.111.32:8080/rest/agile/1.0/board'
-    headers = {'Content-Type':'application/json','cookie':cookie}
+
+def finduser(user):
+    if user == '':
+        return (False,'No user found!')
+    cookie = ''
     try:
-        r = requests.get(url,headers=headers,timeout=3)
-        if r.status_code != 200:
-            return str(r.status_code) + r.text 
-        j = json.loads(r.text)
-        try:
-            return j['warningMessages']
-        except KeyError:
-            pass
-        try:
-            string = dtos(getField(j,None),j['key']) + '\r\n'
-            return string
-        except KeyError as err:
-            return_val = 'given field "{}" not found'.format(err)
-            return return_val
-    except requests.exceptions.RequestException as err:
-        return err
-    
+        cookie = read_cookie()
+    except FileNotFoundError as err:
+        return (False,err)
+    url = 'http://'+domain+'/rest/api/2/user/search'
+    headers = {'Content-Type':'application/json','cookie':cookie}
+    params={'username':user}
+    f,r = send_request(url, method.Get, headers, params, None)
+    if not f:
+        return r
+    j = json.loads(r.text)
+    try:
+        return (True,j[0]['key'])
+    except KeyError:
+        return (False,'No user found!')
+    except IndexError:
+        return (False,'No user found!')
+'''
 def test():
-    #query_number(['sira-21'])
-    #query_assignee([''])
-    #query_assignee(['xp zheng'])
-    #query_project(['Sira'])
-    #query_type(['epic'])
-    #query_sprint(['2'])
-    #query_sprint([''])
-    #query_project_type(['Sirsdf'])
+    query_number(['sira-21'])
+    query_assignee([''])
+    query_assignee(['xp zheng'])
+    query_type(['story'])
+    query_sprint(['2'])
+    query_sprint([''])
+    query_project_type(['Sira','bug'])
     pass
-    '''
-    options = {
-    'server': 'http://10.176.111.32:8080',
-    'cookies': {'JSESSIONID':'DD537C56B9ABD14EEAA710C6BE539644'}
-    }
+
     
-    jira = JIRA(options)
-    '''
-#test()
-#query_project_type(['Sira','bug'])
-#query_project_type(['Sira','task'])
-#query_project_user(['Sira','Hang'])
-#query_project_sprint(['Sira','2'])
-#query_project_user(['Sira','xp Zheng'])
+test()
+query_project_type(['Sira','bug'])
+query_project_type(['Sira','task'])
+query_project_assignee(['Sira','Hang'])
+query_project_sprint(['Sira','2'])
+query_project_assignee(['Sira','xp Zheng'])
+'''

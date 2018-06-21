e3464c45117965ac7eb2668cab176c99f8143264
sailer43 <sailer_cst@yahoo.com>
2018-06-21 17:12:10 +0800

    implemented PgUp and PgDn in completion mode

--- src/view.py
+++ src/view.py
@@ -131,6 +131,14 @@ class SiraApp(App):
     """TODO
     """
 
+    options_per_line = kp.NumericProperty(7)
+    """TODO: both here and class doc
+    """
+
+    page_index = kp.NumericProperty(0)
+    """TODO: both here and class doc
+    """
+
     protected_text = kp.StringProperty(None)
     """Kivy string property to store the protected_text of the current
     command line.
@@ -312,6 +320,15 @@ class SiraApp(App):
         """
         self.commandText.password_mode = True
 
+    def _clear_options(self, instance: AdvancedTextInput) -> None:
+        """TODO: both here and class doc
+        """
+        instance.cancel_selection()
+        start = instance.text.rindex('\n')
+        end = len(instance.text)
+        instance.select_text(start, end)
+        instance.delete_selection()
+
     def _get_font_path(self, font_name) -> str:
         """Private function to search the path of font file based on font_name.
 
@@ -366,7 +383,34 @@ class SiraApp(App):
 
     def _on_display_options(self,
                             instance: AdvancedTextInput,
-                            behavior: str) -> bool:
+                            behavior: str,
+                            option: list) -> bool:
+        """TODO: here and class doc
+        """
+        instance.do_cursor_movement("cursor_end", control=True)
+        cursor = instance.cursor
+        if behavior == "init":
+            self.page_index = 0
+        else:
+            self._clear_options(instance)
+            self.tab_index = -1
+            next_index = self.page_index + self.options_per_line\
+                         if behavior == "next"\
+                         else self.page_index - self.options_per_line
+            self.page_index = max(0, next_index)\
+                              if next_index < len(option)\
+                              else self.page_index
+        end_index = self.page_index + self.options_per_line\
+                    if self.page_index + self.options_per_line <= len(option)\
+                    else len(option)
+        instance.insert_text("\n" + " ".join(option[self.page_index:end_index]))
+        instance.cursor = cursor
+        # calc start indices of displayed options
+        index = 0
+        self.start_indices.clear()
+        for s in option[self.page_index:end_index]:
+            self.start_indices.append(index)
+            index += len(s) + 1
         return True
 
     def _on_font_name(self, value: str) -> None:
@@ -444,7 +488,7 @@ class SiraApp(App):
         # update self.tab_index according to direction
         if direction == "tab":
             self.tab_index = self.tab_index + 1\
-                             if self.tab_index < len(self.option) - 1\
+                             if self.tab_index < len(self.start_indices) - 1\
                              else 0
         elif direction == "left":
             self.tab_index = self.tab_index - 1\
@@ -452,8 +496,8 @@ class SiraApp(App):
                              else 0
         elif direction == "right":
             self.tab_index = self.tab_index + 1\
-                             if self.tab_index < len(self.option) - 1\
-                             else len(self.option) - 1
+                             if self.tab_index < len(self.start_indices) - 1\
+                             else len(self.start_indices) - 1
         # delete and insert next option
         instance.cancel_selection()
         start = self.completion_start
@@ -461,21 +505,17 @@ class SiraApp(App):
         instance.select_text(start, end)
         instance.delete_selection()
         instance.do_cursor_movement("cursor_end", control=True)
-        instance.insert_text(self.option[self.tab_index])
+        instance.insert_text(self.option[self.page_index + self.tab_index])
         # select next option
         last_char_return = instance.text.rindex("\n")
         start = last_char_return + self.start_indices[self.tab_index] + 1
-        end = start + len(self.option[self.tab_index])
+        end = start + len(self.option[self.page_index + self.tab_index])
         instance.select_text(start, end)
 
     def _stop_completion(self, instance: AdvancedTextInput) -> None:
         """TODO: here & class doc
         """
-        instance.cancel_selection()
-        start = instance.text.rindex('\n')
-        end = len(instance.text)
-        instance.select_text(start, end)
-        instance.delete_selection()
+        self._clear_options(instance)
         instance.completion_mode = False
         self.tab_index = -1
         self.option = []
@@ -525,19 +565,11 @@ class SiraApp(App):
         """TODO
         """
         obj = self.commandText
-        if self.option == []:
+        if option == []:
             obj.completion_mode = False
             return
         obj.completion_mode = True
-        obj.do_cursor_movement("cursor_end", control=True)
-        cursor = obj.cursor
-        obj.insert_text("\n" + " ".join(option))
-        obj.cursor = cursor
-        # calc start indices of all options
-        index = 0
-        for s in option:
-            self.start_indices.append(index)
-            index += len(s) + 1
+        self._on_display_options(obj, "init", option)
         # calc the start index of the completion part
         search_start = obj.last_row_start + obj.protected_len
         search_end = obj.last_row_start\
